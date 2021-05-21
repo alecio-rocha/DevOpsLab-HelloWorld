1d171d05ee7ed87f55c3694a0f3a2a0af867c3bd
Alecio Rocha <arocha@daitan.com>
2021-05-21 13:24:34 -0300

    update travis

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Essentals - AROCHA!!!")
+        self.assertRegex(result.data.decode(), "Its ALIVE!!")
 
 
 if __name__ == "__main__":
